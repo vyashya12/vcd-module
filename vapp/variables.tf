82647490488a9067d060d1bb96aad53906b06c6d
Vyashya Raj <86505731+vyashya12@users.noreply.github.com>
2023-12-25 15:11:55 +0800

    Update variables.tf

--- vapp/variables.tf
+++ vapp/variables.tf
@@ -1,8 +1,3 @@
-variable "create_vapp" {
-  description = "If set to true, enable vapp creation"
-  type        = bool
-}
-
 variable "vapp_name" {
   description = "Set vapp name"
   type        = string
